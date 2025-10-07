28bb0fcebaa0e95529b102758ec29b26df15540e
Anna <83037109+anyyaa@users.noreply.github.com>
2025-10-07 15:18:13 +0300

    Create person.java

---
+++ polymorphism/person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Person other = (Person) obj;
+        return age == other.age && name.equals(other.name);
+    }
+}
