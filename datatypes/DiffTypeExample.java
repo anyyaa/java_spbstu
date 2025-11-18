162fd5f3f2c4c5f5dcebc89e9ee47a9fac469ff7
Anna <83037109+anyyaa@users.noreply.github.com>
2025-11-18 11:09:15 +0300

    Create DiffTypeExample.java

---
+++ datatypes/DiffTypeExample.java
@@ -0,0 +1,10 @@
+public class DiffTypeExample {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 3.0;
+        
+        double result = a / b;  // int / double -> double
+        
+        System.out.println(a + " / " + b + " = " + result);
+    }
+}
