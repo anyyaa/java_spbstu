6a7802f670998d9038a7b3dbb69598fd9122b7ab
Anna <83037109+anyyaa@users.noreply.github.com>
2025-11-18 11:12:34 +0300

    Create NarrowingConversion.java

---
+++ datatypes/NarrowingConversion.java
@@ -0,0 +1,11 @@
+public class NarrowingConversion {
+    public static void main(String[] args) {
+        
+        // double -> int (потеря дробной части)
+        double d = 123.456;
+        int i = (int) d;
+        System.out.println("double " + d + " -> int " + i);
+        
+       
+    }
+}
