5db3760c0d363efd63ef41381ed82f2ac36022ba
Anna <83037109+anyyaa@users.noreply.github.com>
2025-11-18 11:05:34 +0300

    Create StringPlusExample.java

---
+++ datatypes/StringPlusExample.java
@@ -0,0 +1,9 @@
+public class StringPlusExample {
+    public static void main(String[] args) {
+        String name = "Anna";
+        boolean isStudent = true;
+
+        String info = name + ", student: " + isStudent;
+        System.out.println(info);
+    }
+}
